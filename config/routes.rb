d9dbfe4e3f4c769d88b181685c2a01e249df67b2
Yass All <yassine.allaouna@gmail.com>
2022-02-18 13:52:42 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  resources :lists do
+    root to: 'lists#index'
+      resources :lists do
     resources :bookmarks, only: [:new, :create]
   end
   delete '/bookmarks/:id', to: 'bookmarks#destroy'
